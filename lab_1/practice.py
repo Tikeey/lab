a24c03af73c22f3cac875b7c3987d67cb036e09e
Timur <kimtimur1999@gmail.com>
2025-01-19 17:13:43 +0500

    Python Comments

--- lab_1/practice.py
+++ lab_1/practice.py
@@ -1,41 +1,28 @@
-#Indentation refers to the spaces at the beginning of a code line.
-
-#Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
-
-#Python uses indentation to indicate a block of code.
-
-#Example
-if 5 > 2:
-  print("Five is greater than two!")
-#Python will give you an error if you skip the indentation:
-
-#Example
-#Syntax Error:
-
-if 5 > 2:
-print("Five is greater than two!")
-#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
-
-#Example
-if 5 > 2:
- print("Five is greater than two!") 
-if 5 > 2:
-        print("Five is greater than two!") 
-#You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
-
-#Example
-#Syntax Error:
-
-if 5 > 2:
- print("Five is greater than two!")
-        print("Five is greater than two!")
- 
- """
- Python Variables
-In Python, variables are created when you assign a value to it:
-
-Example
-Variables in Python:
 """
-x = 5
-y = "Hello, World!"
+Creating a Comment
+Comments starts with a #, and Python will ignore them:
+"""
+#This is a comment
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+#A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Multiline Comments
+#Python does not really have a syntax for multiline comments.
+
+#To add a multiline comment you could insert a # for each line:
+
+#Example
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
