14bb78dd1f4be0d923ddc268cef36311d8196e68
Timur <kimtimur1999@gmail.com>
2025-01-22 22:21:00 +0500

    Python While Loops

--- lab_2/practice.py
+++ lab_2/practice.py
@@ -1,107 +1,43 @@
-"""
-Python supports the usual logical conditions from mathematics:
-
-Equals: a == b
-Not Equals: a != b
-Less than: a < b
-Less than or equal to: a <= b
-Greater than: a > b
-Greater than or equal to: a >= b
-These conditions can be used in several ways, most commonly in "if statements" and loops.
-
-An "if statement" is written by using the if keyword.
-
-ExampleGet your own Python Server
-If statement:
-"""
-a = 33
-b = 200
-if b > a:
-  print("b is greater than a")
-
-  #The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+#With the while loop we can execute a set of statements as long as a condition is true.
 
 #Example
-a = 33
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+#Print i as long as i is less than 6:
 
-  a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+  #With the break statement we can stop the loop even if the while condition is true:
+
+#Example
+#Exit the loop when i is 3:
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+  #With the continue statement we can stop the current iteration, and continue with the next:
+
+#Example
+#Continue to the next iteration if i is 3:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+  #Example
+#Print a message once the condition is false:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
 else:
-  print("a is greater than b")
-
-  #Example
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-else:
-  print("b is not greater than a")
-
-  if a > b: print("a is greater than b")
-
-  a = 2
-b = 330
-print("A") if a > b else print("B")
-
-#Example
-#One line if else statement, with 3 conditions:
-
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
-
-#The and keyword is a logical operator, and is used to combine conditional statements:
-
-#Example
-#Test if a is greater than b, AND if c is greater than a:
-
-a = 200
-b = 33
-c = 500
-if a > b and c > a:
-  print("Both conditions are True")
-
-  #Example
-#Test if a is greater than b, OR if a is greater than c:
-
-a = 200
-b = 33
-c = 500
-if a > b or a > c:
-  print("At least one of the conditions is True")
-
-  #Example
-#Test if a is NOT greater than b:
-
-a = 33
-b = 200
-if not a > b:
-  print("a is NOT greater than b")
-
-  #Example
-x = 41
-
-if x > 10:
-  print("Above ten,")
-  if x > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
-
-    #if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
-
-#Example
-a = 33
-b = 200
-
-if b > a:
-  pass
+  print("i is no longer less than 6")
