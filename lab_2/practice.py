733c59b69879b9cff45c47d86769f4a0734f3407
Timur <kimtimur1999@gmail.com>
2025-01-22 22:18:37 +0500

    Python If ... Else

--- lab_2/practice.py
+++ lab_2/practice.py
@@ -1,350 +1,107 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-#Example
-#Create and print a dictionary:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict)
-
-#Example
-#Print the "brand" value of the dictionary:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict["brand"])
-
-#Duplicate values will overwrite existing values:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
-}
-print(thisdict)
-
-#Example
-#Print the number of items in the dictionary:
-
-print(len(thisdict))
-
-#Example
-#String, int, boolean, and list data types:
-
-thisdict = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
-  "colors": ["red", "white", "blue"]
-}
-
-#Example
-#Print the data type of a dictionary:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(type(thisdict))
-
-#It is also possible to use the dict() constructor to make a dictionary.
-
-#Example
-#Using the dict() method to make a dictionary:
-
-thisdict = dict(name = "John", age = 36, country = "Norway")
-print(thisdict)
-
-#Example
-#Get the value of the "model" key:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-x = thisdict["model"]
-
-#Example
-#Get the value of the "model" key:
-
-x = thisdict.get("model")
-
-#The keys() method will return a list of all the keys in the dictionary.
-
-#Example
-#Get a list of the keys:
-
-x = thisdict.keys()
-
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-
-#x = car.keys()
-
-print(x) #before the change
-
-car["color"] = "white"
-
-print(x) #after the change
-
-#The values() method will return a list of all the values in the dictionary.
-
-#Example
-#Get a list of the values:
-
-x = thisdict.values()
-
-#Get Items
-#The items() method will return each item in a dictionary, as tuples in a list.
-
-#Example
-#Get a list of the key:value pairs
-
-x = thisdict.items()
-
-#Example
-#Check if "model" is present in the dictionary:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-if "model" in thisdict:
-  print("Yes, 'model' is one of the keys in the thisdict dictionary")
-
-  #You can change the value of a specific item by referring to its key name:
-
-#Example
-#Change the "year" to 2018:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict["year"] = 2018
 """
-Update Dictionary
-The update() method will update the dictionary with the items from the given argument.
+Python supports the usual logical conditions from mathematics:
 
-The argument must be a dictionary, or an iterable object with key:value pairs.
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+These conditions can be used in several ways, most commonly in "if statements" and loops.
 
-Example
-Update the "year" of the car by using the update() method:
+An "if statement" is written by using the if keyword.
+
+ExampleGet your own Python Server
+If statement:
 """
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.update({"year": 2020})
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
 
-#Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+  #The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
 
 #Example
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict["color"] = "red"
-print(thisdict)
-"""
-Update Dictionary
-The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
 
-The argument must be a dictionary, or an iterable object with key:value pairs.
-
-Example
-Add a color item to the dictionary by using the update() method:
-"""
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.update({"color": "red"})
-
-#Example
-#The pop() method removes the item with the specified key name:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.pop("model")
-print(thisdict)
-
-#Example
-#The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.popitem()
-print(thisdict)
-
-#Example
-#The del keyword removes the item with the specified key name:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-del thisdict["model"]
-print(thisdict)
-
-#Example
-#The clear() method empties the dictionary:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.clear()
-print(thisdict)
-
-#Example
-#Print all key names in the dictionary, one by one:
-
-for x in thisdict:
-  print(x)
-#Example
-#Print all values in the dictionary, one by one:
-
-for x in thisdict:
-  print(thisdict[x])
-#Example
-#You can also use the values() method to return values of a dictionary:
-
-for x in thisdict.values():
-  print(x)
-#Example
-#You can use the keys() method to return the keys of a dictionary:
-
-for x in thisdict.keys():
-  print(x)
-#Example
-#Loop through both keys and values, by using the items() method:
-
-for x, y in thisdict.items():
-  print(x, y)
+  a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
 
   #Example
-#Make a copy of a dictionary with the copy() method:
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-mydict = thisdict.copy()
-print(mydict)
-#Another way to make a copy is to use the built-in function dict().
+  if a > b: print("a is greater than b")
 
-#Example
-#Make a copy of a dictionary with the dict() function:
-
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-mydict = dict(thisdict)
-print(mydict)
+  a = 2
+b = 330
+print("A") if a > b else print("B")
 
 #Example
-#Create a dictionary that contain three dictionaries:
+#One line if else statement, with 3 conditions:
 
-myfamily = {
-  "child1" : {
-    "name" : "Emil",
-    "year" : 2004
-  },
-  "child2" : {
-    "name" : "Tobias",
-    "year" : 2007
-  },
-  "child3" : {
-    "name" : "Linus",
-    "year" : 2011
-  }
-}
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
 
-#Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
-
-child1 = {
-  "name" : "Emil",
-  "year" : 2004
-}
-child2 = {
-  "name" : "Tobias",
-  "year" : 2007
-}
-child3 = {
-  "name" : "Linus",
-  "year" : 2011
-}
-
-myfamily = {
-  "child1" : child1,
-  "child2" : child2,
-  "child3" : child3
-}
-
-#To access items from a nested dictionary, you use the name of the dictionaries, starting with the outer dictionary:
+#The and keyword is a logical operator, and is used to combine conditional statements:
 
 #Example
-#Print the name of child 2:
+#Test if a is greater than b, AND if c is greater than a:
 
-print(myfamily["child2"]["name"])
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
 
-#You can loop through a dictionary by using the items() method like this:
+  #Example
+#Test if a is greater than b, OR if a is greater than c:
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+  #Example
+#Test if a is NOT greater than b:
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+  #Example
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+    #if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
 
 #Example
-#Loop through the keys and values of all nested dictionaries:
+a = 33
+b = 200
 
-for x, obj in myfamily.items():
-  print(x)
-
-  for y in obj:
-    print(y + ':', obj[y])
-
-"""
-    Dictionary Methods
-Python has a set of built-in methods that you can use on dictionaries.
-
-Method	Description
-clear()	Removes all the elements from the dictionary
-copy()	Returns a copy of the dictionary
-fromkeys()	Returns a dictionary with the specified keys and value
-get()	Returns the value of the specified key
-items()	Returns a list containing a tuple for each key value pair
-keys()	Returns a list containing the dictionary's keys
-pop()	Removes the element with the specified key
-popitem()	Removes the last inserted key-value pair
-setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
-update()	Updates the dictionary with the specified key-value pairs
-values()	Returns a list of all the values in the dictionary
-"""
+if b > a:
+  pass
